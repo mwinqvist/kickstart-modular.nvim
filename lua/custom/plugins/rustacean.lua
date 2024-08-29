18c3a410c48f96239a6f1bba316ba2cf85ab8d8a
Martin Winqvist <winqvist.martin@gmail.com>
2024-08-29 20:25:20 +0200

    Add rustacean plugin

---
+++ lua/custom/plugins/rustacean.lua
@@ -0,0 +1,8 @@
+-- rustacean.nvim
+-- https://github.com/mrcjkb/rustaceanvim
+
+return {
+  'mrcjkb/rustaceanvim',
+  version = '^5', -- Recommended
+  lazy = false, -- This plugin is already lazy
+}
