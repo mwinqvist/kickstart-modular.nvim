816567b4930d8fdfd3fc1bfae9ed70e06731933c
Martin Winqvist <winqvist.martin@gmail.com>
2024-09-16 09:05:38 +0200

    Add neogit, remove fugitive

---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,13 @@
+-- neogit.nvim
+-- https://github.com/NeogitOrg/neogit
+
+return {
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'sindrets/diffview.nvim',
+
+    'nvim-telescope/telescope.nvim',
+  },
+  config = true,
+}
