3d6ed6a9c4200906eb85d59f0d49d9688424de61
Martin Winqvist <winqvist.martin@gmail.com>
2024-09-24 12:20:02 +0200

    Change keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,26 +1,18 @@
 -- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
 
 -- Clear highlights on search when pressing 'q' in normal mode
---  See `:help hlsearch`
 vim.keymap.set('n', 'q', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- Disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- Use 'kj' instead of Escape key
+vim.keymap.set({ 'v', 'i' }, 'kj', '<Esc>')
+vim.keymap.set('c', 'kj', '<C-c>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -30,10 +22,6 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
--- Use 'kj' instead of Escape key
-vim.keymap.set({ 'v', 'i' }, 'kj', '<Esc>')
-vim.keymap.set('c', 'kj', '<C-c>')
 
 -- Use Tab and S-Tab to indent/outdent
 -- Remapping Tab also remaps 'i'...
@@ -48,15 +36,18 @@ vim.keymap.set('n', '<C-p>', '<C-o>', opts)
 vim.keymap.set('n', '<C-n>', '<C-i>', opts)
 
 -- Use 'H', 'L' to move to start/end of line
-vim.keymap.set({ 'n', 'v' }, 'H', '^', opts)
-vim.keymap.set({ 'n', 'v' }, 'L', '$', opts)
+vim.keymap.set({ 'n', 'v', 'o' }, 'H', '^', opts)
+vim.keymap.set({ 'n', 'v', 'o' }, 'L', '$', opts)
 
 -- Use 'J','K' to move down/up
-vim.keymap.set('n', 'J', '<C-d>', opts)
-vim.keymap.set('n', 'K', '<C-u>', opts)
+vim.keymap.set('n', 'J', '}', opts)
+vim.keymap.set('n', 'K', '{', opts)
 
 -- Use 'ä' to create marks
 -- 'm' is used for cut in cutlass.nvim
 vim.keymap.set('n', 'ä', 'm', opts)
+
+-- Use 'U' to redo
+vim.keymap.set('n', 'U', '<C-r>', opts)
 
 -- vim: ts=2 sts=2 sw=2 et
