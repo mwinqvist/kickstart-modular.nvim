19c2d61952a3161ab0fd0c4c290e33c44f6ea4df
Martin Winqvist <winqvist.martin@gmail.com>
2024-10-02 09:09:21 +0200

    Add diagnostic popup and bash lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -208,6 +208,7 @@ return {
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        bashls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -254,6 +255,7 @@ return {
         'isort',
         'markdownlint',
         'rustfmt',
+        'shellcheck',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
