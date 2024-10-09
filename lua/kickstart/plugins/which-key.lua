1f645ad2f71098a4008bd8f8f0a1e6cb6ac726e7
Martin Winqvist <winqvist.martin@gmail.com>
2024-10-09 10:22:10 +0200

    Add keymaps for telescope git pickers

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -61,6 +61,7 @@ return {
         { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
+        { '<leader>sv', group = '[S]earch [V]ersion Control' },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
